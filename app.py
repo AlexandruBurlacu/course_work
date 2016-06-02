fecd6a0b36deb7b893ef198790ea7a805c42ae97
Alex Burlacu <alexburlacu96@gmail.com>
2016-06-02 21:27:17 +0000

    new commit

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
     
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug = True)
