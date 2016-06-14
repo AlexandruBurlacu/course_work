b9282b8ea77291284fb3f19122b189c1aa079721
AlexandruBurlacu <alexburlacu96@gmail.com>
2016-06-14 17:27:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import neuralnet
 import activators
+import csv_utils
+import preprocess
+
+import random
 
 # Features & Labels
 features = np.array([[0, 0, 1],
@@ -10,5 +14,10 @@ features = np.array([[0, 0, 1],
 
 labels = np.array([[0, 1, 1, 0]]).T
 
+data = csv_utils.csv_read('breeds.csv')
+
+out_obj = preprocess.DataPreprocessor(data)
+features, labels = out_obj.clear()
+    
 if __name__ == '__main__':
-	print("Final result: ", neuralnet.neuralnet(features, labels))
+	print("Final result: ", neuralnet.neuralnet(np.array(features)[::200], np.array(labels)[::200]))
