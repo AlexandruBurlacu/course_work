74431fb197599a3f96032ceedf9d54b5f33e4b48
AlexandruBurlacu <alexburlacu96@gmail.com>
2016-06-14 20:30:28 +0300

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,4 @@
-# TODO SGD or minibatch-GD and adaptive alpha
+# TODO adaptive alpha
 
 import numpy as np
 from activators import logistic
@@ -6,7 +6,14 @@ np.random.seed(1)
 
 
 
-def neuralnet(features, labels, alpha = 0.5, hidden_layer = 4, iters = 10000, show_err = False, activation_f = logistic):
+def neuralnet(features,
+			  labels,
+			  alpha = 0.5,
+			  hidden_layer = 4,
+			  iters = 50000,
+			  show_err = False,
+			  activation_f = logistic,
+			  batch_size = 10):
 	"""
 		This function trains the ANN and then
 		computes the labels for given features.
@@ -21,25 +28,26 @@ def neuralnet(features, labels, alpha = 0.5, hidden_layer = 4, iters = 10000, sh
 	synapse0 = 2 * np.random.random((in_layer, hidden_layer)) - 1
 	synapse1 = 2 * np.random.random((hidden_layer, out_layer)) - 1
 	
-	
 	for i in range(iters):
-	
-		layer0 = features
+		# mini - batch GD
+		sgd_index = np.random.randint(len(features) - batch_size)
+		
+		layer0 = features[sgd_index:sgd_index + batch_size]
 		layer1 = activation_f(np.dot(layer0, synapse0))
 		layer2 = activation_f(np.dot(layer1, synapse1))
-	
-		# Backpropagation
-		l2_error = layer2 - labels
+		
+		# Computing the errors
+		l2_error = layer2 - labels[sgd_index:sgd_index + batch_size]
 		d_layer2 = l2_error * activation_f(layer2, dS = True)
 		
 		l1_error = d_layer2.dot(synapse1.T)
 		d_layer1 = l1_error * activation_f(layer1, dS = True)
 	
-		# Gradient Descent
+		# Gradient Descent aka Backpropagation
 		synapse1 -= alpha * layer1.T.dot(d_layer2)
 		synapse0 -= alpha * layer0.T.dot(d_layer1)
 		
 		if show_err:
 			if i % 1000 == 0: print("Error: ", np.mean(np.abs(l2_error)) * 100, "%")
-	
 	return layer2
+
