2a58db1ed1eb7b9f0b92504f6baecbd179fa3296
AlexandruBurlacu <alexburlacu96@gmail.com>
2016-06-14 22:11:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ import neuralnet
 import activators
 import csv_utils
 import preprocess
+import csv
 
 def add_bias(seq):
 	return list(seq) + [1]
+
 
 data = csv_utils.csv_read('breeds.csv')
 
 out_obj = preprocess.DataPreprocessor(data)
 features, labels = out_obj.clear()
-    
-if __name__ == '__main__':
-	print("Final result: ",
-				neuralnet.neuralnet(np.array(map(add_bias, features), dtype = np.float16),
-									np.array(labels, dtype = np.float16),
-									show_err = True, alpha = 0.5, hidden_layer = 32))
 
+
+def csv_write(param, data, filename = 'breeds.csv'):
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(param)
+        for row in data:
+            writer.writerow(row)
+
+# csv_write(("male weight", "female weight", "male height", "female height", "breed"), features, filename = "normalbreeds.csv")
+
+
+if __name__ == '__main__':
+	model = neuralnet.neuralnet_train(np.array(map(add_bias, features), dtype = np.float16),
+						np.array(labels, dtype = np.float16),
+						show_err = False, alpha = 0.5, hidden_layer = 32, iters = 80000)
+	data = [0.758, 0.8041, 0.65, 0.766, 1]
+	prediction = preprocess.vector_to_label((map(round, neuralnet.neuralnet_predict(data, model))))
+	
+	print(prediction)
--- neuralnet.py
+++ neuralnet.py
@@ -1,12 +1,11 @@
 # TODO adaptive alpha
-
 import numpy as np
 from activators import logistic
 np.random.seed(1)
 
 
 
-def neuralnet(features,
+def neuralnet_train(features,
 			  labels,
 			  alpha = 0.5,
 			  hidden_layer = 4,
@@ -20,9 +19,7 @@ def neuralnet(features,
 	"""
 	
 	in_layer = len(features[0])
-	print(in_layer)
 	out_layer = len(labels[0])
-	print(out_layer)
 	
 	#synapses
 	synapse0 = 2 * np.random.random((in_layer, hidden_layer)) - 1
@@ -42,12 +39,27 @@ def neuralnet(features,
 		
 		l1_error = d_layer2.dot(synapse1.T)
 		d_layer1 = l1_error * activation_f(layer1, dS = True)
-	
+		
 		# Gradient Descent aka Backpropagation
 		synapse1 -= alpha * layer1.T.dot(d_layer2)
 		synapse0 -= alpha * layer0.T.dot(d_layer1)
 		
+		err = np.mean(np.abs(l2_error))
 		if show_err:
-			if i % 1000 == 0: print("Error: ", np.mean(np.abs(l2_error)) * 100, "%")
-	return layer2
+			if i % 1000 == 0: print("Error: ", err * 100, "%")
+		
+	return synapse0, synapse1, activation_f
 
+
+def neuralnet_predict(features, model):
+	"""
+		Given a trained model (synapses and the activation function)
+		and features it returns the estimated label.
+	"""
+	synapse0, synapse1, activation_f = model
+	layer0 = features
+	layer1 = activation_f(np.dot(layer0, synapse0))
+	layer2 = activation_f(np.dot(layer1, synapse1))
+	
+	return layer2
+	
